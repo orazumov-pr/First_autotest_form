d11b965a0bbfd71dffae2d641f168883657e8288
Oleg Razumov <orazumoff@gmail.com>
2026-02-14 19:46:13 +0300

    Delete @BeforeAll

--- src/main/java/tests/PracticeFormTests.java
+++ src/main/java/tests/PracticeFormTests.java
@@ -1,8 +1,5 @@
 package tests;
 
-
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -12,16 +9,7 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeFormTests {
 
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
-    }
-
-    @Test
+   @Test
     void fillFormTest() {
         open("/automation-practice-form");
 
